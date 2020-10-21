a930ddc368fbea7ff4a4a55fe767ec95e4fdf8aa
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2020-10-21 18:01:50 -0200

    deu certo o estilo

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php 
 function carrega_estilos(){
-  wp_enqueue_style('estilos-pai',get_template_directory_uri() .'/style.css');
+  wp_enqueue_style('estilos-pai', get_template_directory_uri() . '/style.css');
 
 }
 add_action('wp_enqueue_scripts','carrega_estilos');
  ?>
+ 
